24f4f82dae3aba3076131973c895159ac5520bbc
Tim <Tim>
2017-01-22 10:10:23 +0300

    add ckeditor on article.html

--- src/main/java/com/linoge/config/SecurityConfig.java
+++ src/main/java/com/linoge/config/SecurityConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 /**
  * Created by Tim on 08.01.2017.
@@ -36,25 +38,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/getuser", "/gettags", "/createuser").permitAll()
-                .anyRequest().hasRole("ADMIN")
-                .and()
+                .antMatchers("/getuser", "/gettags", "/createuser",
+                         "/js/**", "/css/**","/templates/**","/favicon.ico", "/index", "/").permitAll()
+                .anyRequest().hasRole("ADMIN").and()
                 .formLogin()
-                .loginProcessingUrl("/login")
-                .successHandler(successAuthenticationHandler)
-                .failureHandler(failureAuthenticationHandler)
-                .usernameParameter("username")
-                .passwordParameter("password")
+                .loginPage("/login").permitAll()
                 .and()
                 .logout()
-                .logoutUrl("/logout")
+                .logoutUrl("/logout").permitAll()
                 .and().csrf().disable();
     }
-
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(bcryptPasswordEncoder());
     }
+
+
 }
---
+++ src/main/java/com/linoge/controllers/template/LoginController.java
@@ -0,0 +1,23 @@
+package com.linoge.controllers.template;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Tim on 21.01.2017.
+ */
+@Controller
+public class LoginController {
+    @RequestMapping("/login")
+    public String getLogin(@RequestParam(value = "error", required = false) String error,
+                           @RequestParam(value = "logout", required = false) String logout,
+                           Model model){
+
+        model.addAttribute("error", error != null);
+        model.addAttribute("logout", logout != null);
+        return "login";
+    }
+}
