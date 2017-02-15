a14f36df669d05fad4d500ecab9f5f427b3b0e26
apivanova <alex.ivanova.p@gmail.com>
2017-02-15 23:28:43 +0300

    added close icon to tags in article creator

--- src/main/resources/static/js/Tag.js
+++ src/main/resources/static/js/Tag.js
@@ -5,10 +5,16 @@ var Tag = {
         var tagTitle = $("#tags option:selected").val();
         var tag = _.find(this.tagList, {title: tagTitle});
         if (!_.includes(this.selectedTags, tag.id)) {
-            this.selectedTags.push({"id":tag.id});
-            $( "#btnList" ).append( '<a href="#" style="font-size: 8pt;">' + tagTitle + '</a>&nbsp&nbsp' );
+            this.selectedTags.push({"id": tag.id});
+            $("#btnList").append('<span class="tag label label-info"><span>' + tagTitle + '</span>' +
+                '<a><i class="remove glyphicon glyphicon-remove-sign glyphicon-white"></i></a></span>');
+            console.log(this.selectedTags);
         }
-        console.log(this.selectedTags);
+    },
+
+    deleteTagFromList: function(){
+        var tagTitle = $(this).text();
+        console.log(tagTitle);
     },
 
     getTags: function () {
