b34767ec92214c197bba946004b1be48e0b57e76
Linoge <lyahor.timofei@gmail.com>
2017-02-25 18:46:20 +0200

    Set CacheConfig more simple. Add cache statistic.

--- src/main/java/com/linoge/config/CacheConfig.java
+++ src/main/java/com/linoge/config/CacheConfig.java
@@ -1,12 +1,9 @@
 package com.linoge.config;
 
-import org.springframework.cache.CacheManager;
+import net.sf.ehcache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.ehcache.EhCacheCacheManager;
-import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 /**
  * Created by Timo on 21.02.2017.
@@ -15,18 +12,10 @@ import org.springframework.core.io.ClassPathResource;
 @EnableCaching
 public class CacheConfig {
 
-    private static final String XML_PATH = "ehcache.xml";
+    private static final String XML_PATH = "src/main/resources/ehcache.xml";
 
     @Bean
     public CacheManager ehCacheManager() {
-        return new EhCacheCacheManager(ehCacheCacheManager().getObject());
-    }
-
-    @Bean
-    public EhCacheManagerFactoryBean ehCacheCacheManager() {
-        EhCacheManagerFactoryBean cmfb = new EhCacheManagerFactoryBean();
-        cmfb.setConfigLocation(new ClassPathResource(XML_PATH));
-        cmfb.setShared(true);
-        return cmfb;
+        return CacheManager.create(XML_PATH);
     }
 }
