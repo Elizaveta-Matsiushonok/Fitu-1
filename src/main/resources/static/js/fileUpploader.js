a8ec70b9c33bc824ce8ae9ea4518c41c06da3655
apivanova <alex.ivanova.p@gmail.com>
2017-02-28 01:08:30 +0300

    Added uploading button to article creator.

--- src/main/resources/static/js/fileUpploader.js
+++ src/main/resources/static/js/fileUpploader.js
@@ -1,12 +1,17 @@
-/**
- * Created by Timo on 14.02.2017.
- */
-function uploadFormData(){
-    $('#result').html('');
+
+function uploadFormData(elem) {
+    // $('#result').html('');
+    var currentId = $(elem).attr("id");
+    var currentNum = currentId.slice(-1);
     var myForm = new FormData();
-    for (i = 0; i < file.files.length; i++) {
-        myForm.append("file" + i, file.files[i]);
+
+    // var currentFile = $('#file' + currentNum).attr('name');
+    // console.log(currentFile);
+    var files = document.getElementById("file" + currentNum);
+    for (i = 0; i < files.files.length; i++) {
+        myForm.append("file" + i, files.files[i]);
     }
+    currentNum++;
 
     $.ajax({
         url: 'http://localhost:8081/upload',
@@ -15,9 +20,21 @@ function uploadFormData(){
         processData: false,
         contentType: false,
         type: 'POST',
-        success: function(data){
-            //  $('#result').html(data+ " uploaded by FormData!");
-            $('#result').html(data);
+        success: function (data) {
+
+            $("#imageList").append('<div class="row">'+
+                '<form id="form' + currentNum + '" method="POST" enctype="multipart/form-data" action="/upload" class="col-xs-6 col-sm-3 col-lg-3">' +
+                '<input type="file" multiple="multiple" name="file' + currentNum +'[]" id="file' + currentNum +'"/>' +
+                '</form> <button  id ="uploadBtn' +  currentNum +'" value="Submit" onclick="uploadFormData(this)" style="width: 55px" class="col-xs-6 col-sm-3 col-lg-3">Upload</button>' +
+                '<button id="clearBtn' + currentNum +' " value="Submit" onclick="deleteImage(this)" style="margin-left: 10px; width: 50px" class="col-xs-6 col-sm-3 col-lg-3">Clear</button></div>');
         }
     });
+}
+
+function deleteImage(elem) {
+    var currentId = $(elem).attr("id");
+    var currentNum = currentId.slice(-1);
+    console.log(currentId);
+    console.log(currentNum);
+    $('#file' + currentNum).val('');
 }
