6540ffb6e6edb8e69debea252a0dad7326dd18b7
Linoge <lyahor.timofei@gmail.com>
2017-03-07 19:56:36 +0200

    Add builder

--- src/main/java/com/linoge/models/converters/ArticleConverter.java
+++ src/main/java/com/linoge/models/converters/ArticleConverter.java
@@ -36,7 +36,19 @@ public final class ArticleConverter {
                         .tags(convertTagsCollectionToDTO(element.getTags()))
                         .build())
                 .collect(toList());
+
+
     }
+
+    public static void main(String[] args) {
+
+        Article article = Article.builder().header("header").id(42L).title("Hello wordl").build();
+        ArticleDTO tmp = new Builder(article).header().id().build();
+        System.out.println(tmp);
+    }
+
+//    buildCollection(articles).title().body().tags();
+//    buildSingle(article).title().body().tags();
 
     public static String getHeader(String text) {
         return text.substring(0, text.indexOf(BODY_TAG));
@@ -44,6 +56,48 @@ public final class ArticleConverter {
 
     public static String getBody(String text) {
         return text.substring(text.indexOf(BODY_TAG) + BODY_TAG.length());
+    }
+
+    void buildSingle(Article article) {
+        ArticleDTO.builder()
+                .id(article.getId())
+                .title(article.getTitle())
+                .header(article.getHeader())
+                .tags(convertTagsCollectionToDTO(article.getTags()));
+    }
+
+    public static class Builder {
+        private final Article article;
+        private final ArticleDTO.ArticleDTOBuilder buidler = ArticleDTO.builder();
+//        private final ArticleDTO dto = ArticleDTO.builder();
+
+        public Builder(Article article) {
+            this.article = article;
+        }
+
+        public Builder title() {
+            buidler.title(article.getTitle());
+            return this;
+        }
+
+        public Builder header() {
+            buidler.header(article.getHeader());
+            return this;
+        }
+
+        public Builder id() {
+            buidler.id(article.getId());
+            return this;
+        }
+
+        public Builder body() {
+            buidler.title(article.getTitle());
+            return this;
+        }
+
+        public ArticleDTO build() {
+            return buidler.build();
+        }
     }
 
 
