f2c0a5930d6d16f0a96405ddae52e91bf62792ac
apivanova <alex.ivanova.p@gmail.com>
2017-03-22 23:07:19 +0300

    added images in row.

--- src/main/resources/static/js/Image.js
+++ src/main/resources/static/js/Image.js
@@ -22,23 +22,20 @@ var Image = {
     },
 
     displayImageArray: function (imageArray) {
-        $('#imageList').html('');
+        $('#deleteBtnArray').html('');
         _.forEach(imageArray, function (object){
             var index = _.findIndex(imageArray, (function(obj){
                 return obj.id == object.id;
             }));
-            $('#result').append('<div id="image' + index + '">' +
-                '<div id="img' + index +'"><div class="col-md-4 col-sm-4 col-xs-12">' +
-                '<a href="#portfolioBig2"  data-toggle="modal">' + object.path + '</a></div></div></div>');
+            $('#result').append('<div class="img-div"><div id="image' + index + '" >' +
+                '<div id="img' + index +'"><div><a data-toggle="modal">' + object.path + '</a></div></div></div>' +
+                '<button id="deleteImageBtn' +index +'" type="Submit" onclick="Image.deleteImage(this)">Delete</button></div>');
             $('img').addClass('img-responsive');
-            $('#imageList').append('<div><button id="deleteImageBtn' +index +'" type="Submit" onclick="Image.deleteImage(this)"'+
-                'class="col-xs-6 col-sm-3 col-lg-3" style="float: left">Delete</button></div>');
         });
     },
 
     deleteImage: function (elem) {
         $('#file').val('');
-        $('#imageList').html('');
         var index = $(elem).attr('id').substr(-1);
         var imageId = Image.imageArray[index].id;
         $.ajax({
